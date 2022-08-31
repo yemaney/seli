9e6d8cf9049c1068512a9d83cdec111cf205bf1b
yemaney <yemane_@outlook.com>
2022-08-30 22:50:25 -0400

    ♻️ : main use browser worker on browser jobs

--- seli/main.py
+++ seli/main.py
@@ -1,11 +1,16 @@
 import sys
 
 from seli.config_readers import read_jobs
+from seli.workers import browser_worker
 
 
 def main():
     jobs_filepath = sys.argv[1]
-    read_jobs(jobs_filepath)
+    jobs = read_jobs(jobs_filepath)
+
+    for job in jobs:
+        if job["kind"] == "browser":
+            browser_worker(job["label"])
 
 
 if __name__ == "__main__":
